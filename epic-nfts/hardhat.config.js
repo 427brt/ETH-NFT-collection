a5927320abcbfd456bfe0e90e37e8f756e6fe374
427brt <yu0427suzuki@icloud.com>
2022-10-09 02:27:01 +0900

    NFTをopensea_testnetで確認

--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/9Rw1lTV7yYTbaDBYuL2RNSbbeirDW1xy",
+      accounts: ["f53f4a2741ddf0357677a5f1be368516453f7b8290af3ff004dfd20a38bfd31e"]
+    },
+  },
 };
